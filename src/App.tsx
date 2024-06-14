1cc7bacb305c5f76e66bc142532cf361f901ac06
sap <sabark@eluocnc.com>
2024-06-14 12:20:01 +0900

    ci/cd test

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>여기 수정했지롱 ㅎㅎ</p>
+        <p>여기 수정했지롱 ㅎㅎzz</p>
         <a
           className="App-link"
           href="https://reactjs.org"
